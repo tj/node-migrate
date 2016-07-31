97202d9ba79407a021586d599c1fefae302bb8c0
Andres Olave <andres.olave@thinkingbytes.net>
2016-07-31 19:08:10 +0300

    Split node-migrate & the filestore Reusable tests change migration script

---
+++ test/common/issue-33.js
@@ -0,0 +1,53 @@
+
+var assert = require('assert');
+
+var migrate = require('../..');
+var db = require('./fixtures/db');
+
+var A1 = ['1-up', '2-up', '3-up'];
+var A2 = A1.concat(['3-down', '2-down', '1-down']);
+
+module.exports = function(BASE, storeUnderTest) {
+  return function () {
+    var set;
+    var store;
+
+    beforeEach(function () {
+      var Store = require('node-migrate-' + storeUnderTest.storeType + 'store');
+      store = new (Function.prototype.bind.apply(Store, [null].concat(storeUnderTest.args)));
+      set = migrate.load(store, BASE);
+    });
+
+    it('should run migrations in the correct order', function (done) {
+
+      set.up(function (err) {
+        assert.ifError(err);
+        assert.deepEqual(db.issue33, A1);
+
+        set.up(function (err) {
+          assert.ifError(err);
+          assert.deepEqual(db.issue33, A1);
+
+          set.down(function (err) {
+            assert.ifError(err);
+            assert.deepEqual(db.issue33, A2);
+
+            set.down(function (err) {
+              assert.ifError(err);
+              assert.deepEqual(db.issue33, A2);
+
+              done();
+            });
+          });
+        });
+      });
+
+    });
+
+    afterEach(function (done) {
+      db.nuke();
+      store.reset(done);
+    });
+
+  }
+};
---
+++ test/index.js
@@ -0,0 +1,14 @@
+var path = require('path');
+
+var getStoreUnderTest = function(base) {
+  var STATE = path.join(base, '.migrate');
+  return {storeType: 'file', args: [{stateFile: STATE}]}
+};
+
+var BASIC_BASE = path.join(__dirname, 'common', 'fixtures', 'basic');
+var basicTests = require('./common/basic');
+describe('basic migration', basicTests(BASIC_BASE, getStoreUnderTest(BASIC_BASE)));
+
+var ISSUE33_BASE = path.join(__dirname, 'common', 'fixtures', 'issue-33');
+var issue33 = require('./common/issue-33');
+describe('issue-33', issue33(ISSUE33_BASE, getStoreUnderTest(ISSUE33_BASE)));
