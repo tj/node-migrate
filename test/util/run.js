2a77217b0a7a50427e7522d88965a4d10384b8ba
Wes Todd <wes@wesleytodd.com>
2017-04-17 18:53:53 -0500

    more robust errors on create command

--- test/util/run.js
+++ test/util/run.js
@@ -1,10 +1,8 @@
 'use strict'
 const spawn = require('child_process').spawn
-const assert = require('assert')
 
 module.exports = function run (cmd, dir, args, done) {
   var p = spawn(cmd, ['-c', dir, ...args])
-
   var out = ''
   p.stdout.on('data', function (d) {
     out += d.toString('utf8')
@@ -14,10 +12,6 @@ module.exports = function run (cmd, dir, args, done) {
   })
   p.on('error', done)
   p.on('close', function (code) {
-    if (code !== 0) {
-      console.log(out)
-    }
-    assert.equal(code, 0)
-    done(null, out)
+    done(null, out, code)
   })
 }
