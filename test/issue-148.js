ac66ebe7c5c740c633ee2ca66744f1ee15e66626
Akiva Gordon <akivag29@gmail.com>
2019-02-21 17:07:24 -0500

    Add test to ensure that relative migration dir still works

--- test/issue-148.js
+++ test/issue-148.js
@@ -3,25 +3,51 @@
 const path = require('path')
 const assert = require('assert')
 const rimraf = require('rimraf')
+const fs = require('fs')
 const os = require('os')
 const db = require('./util/db')
 const run = require('./util/run')
 
 const ABSOLUTE_DIR = path.join(os.tmpdir(), 'issue-#148')
 
-function reset () {
-  rimraf.sync(ABSOLUTE_DIR)
-  db.nuke()
-}
-
 describe('issue #148 - `migrate create --migrations-dir=... foo` should allow an absolute path', () => {
+  let absolutePath
+
+  function reset () {
+    if (!absolutePath) return
+
+    rimraf.sync(absolutePath)
+    db.nuke()
+  }
+
   beforeEach(reset)
   afterEach(reset)
 
   it('should allow an absolute path', (done) => {
-    run.create(path.resolve(), ['--migrations-dir', ABSOLUTE_DIR, 'foo'], (err, out, code) => {
+    absolutePath = ABSOLUTE_DIR
+
+    run.create(path.resolve(), ['--migrations-dir', absolutePath, 'foo'], (err, out, code) => {
       assert.ifError(err)
       assert.equal(code, 0)
+
+      done()
+    })
+  })
+
+  it('should still allow a relative path', (done) => {
+    const relativeDir = 'fixtures/issue-148'
+
+    absolutePath = path.join(__dirname, relativeDir)
+
+    run.create(__dirname, ['--migrations-dir', relativeDir, 'foo'], (err, out, code) => {
+      assert.ifError(err)
+      assert.equal(code, 0)
+
+      const atLeastOneFileWasCreatedInRelativeDir = fs
+        .readdirSync(absolutePath)
+        .length > 0
+
+      assert(atLeastOneFileWasCreatedInRelativeDir)
 
       done()
     })
