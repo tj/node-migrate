018e9832ddfe322d953308db3beae16c7b84dbf4
Akiva Gordon <akivag29@gmail.com>
2019-06-01 21:20:49 -0700

    closes #149 allow abs path for --migrations-dir

---
+++ test/issue-148.js
@@ -0,0 +1,74 @@
+/* eslint-env mocha */
+'use strict'
+const path = require('path')
+const assert = require('assert')
+const rimraf = require('rimraf')
+const mkdirp = require('mkdirp')
+const fs = require('fs')
+const { create, up, down, init } = require('./util/run')
+
+const TMP_DIR = path.join(__dirname, 'fixtures', 'tmp')
+
+describe('issue #148 - `migrate create --migrations-dir=... foo` should allow an absolute path', () => {
+  function reset () {
+    rimraf.sync(TMP_DIR)
+    mkdirp(TMP_DIR)
+  }
+
+  beforeEach(reset)
+  afterEach(reset)
+
+  it('should allow an absolute path', (done) => {
+    init(TMP_DIR, ['--migrations-dir', path.join(TMP_DIR, 'other')], (err, out, code) => {
+      assert.ifError(err)
+      assert.equal(code, 0, out)
+      assert.doesNotThrow(() => {
+        fs.accessSync(path.join(TMP_DIR, 'other'))
+      })
+
+      create(TMP_DIR, ['--migrations-dir', path.join(TMP_DIR, 'other'), 'foo'], (err, out, code) => {
+        assert.ifError(err)
+        assert.equal(code, 0, out)
+
+        up(TMP_DIR, ['--migrations-dir', path.join(TMP_DIR, 'other')], (err, out, code) => {
+          assert.ifError(err)
+          assert.equal(code, 0, out)
+
+          down(TMP_DIR, ['--migrations-dir', path.join(TMP_DIR, 'other')], (err, out, code) => {
+            assert.ifError(err)
+            assert.equal(code, 0, out)
+
+            done()
+          })
+        })
+      })
+    })
+  })
+
+  it('should still allow a relative path', (done) => {
+    init(TMP_DIR, ['--migrations-dir', 'other'], (err, out, code) => {
+      assert.ifError(err)
+      assert.equal(code, 0, out)
+      assert.doesNotThrow(() => {
+        fs.accessSync(path.join(TMP_DIR, 'other'))
+      })
+
+      create(TMP_DIR, ['--migrations-dir', 'other', 'foo'], (err, out, code) => {
+        assert.ifError(err)
+        assert.equal(code, 0)
+
+        up(TMP_DIR, ['--migrations-dir', 'other'], (err, out, code) => {
+          assert.ifError(err)
+          assert.equal(code, 0, out)
+
+          down(TMP_DIR, ['--migrations-dir', 'other'], (err, out, code) => {
+            assert.ifError(err)
+            assert.equal(code, 0, out)
+
+            done()
+          })
+        })
+      })
+    })
+  })
+})
