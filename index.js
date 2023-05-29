f5efb1fd421a0295b7cb3d3cabe564b86816be06
Wes Todd <wes@wesleytodd.com>
2023-05-29 11:21:59 -0500

    fix(deps): standard@17.0.0

--- index.js
+++ index.js
@@ -51,8 +51,8 @@ exports.load = function (options, fn) {
   const set = new MigrationSet(store)
 
   loadMigrationsIntoSet({
-    set: set,
-    store: store,
+    set,
+    store,
     migrationsDirectory: opts.migrationsDirectory,
     filterFunction: opts.filterFunction,
     sortFunction: opts.sortFunction,
